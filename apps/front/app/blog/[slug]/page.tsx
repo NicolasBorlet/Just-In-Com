e1c54bee2626a8d91427fd0c25fb6b1ffb70647f
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-06-12 17:33:27 +0200

    Refactor article data handling by removing ArticleClient component; streamline Article page to directly fetch and display article data based on locale.

--- apps/front/app/blog/[slug]/page.tsx
+++ apps/front/app/blog/[slug]/page.tsx
@@ -1,12 +1,27 @@
+'use client'
+
+import { useLocale } from "@/contexts/LocaleContext";
 import { getArticle } from "@/data/loaders";
-import ArticleClient from "./ArticleClient";
+import ArticlePage from "@/pages/ArticlePage";
+import type { Article } from "@/types";
+import { useEffect, useState } from "react";
 
-export default async function Article({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const data = await getArticle(params.slug, "fr");
+export default function Article({ slug }: { slug: string }) {
+    const { locale } = useLocale();
+    const [data, setData] = useState<Article | null>(null);
 
-  return <ArticleClient initialData={data} slug={params.slug} />;
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await getArticle(slug, locale);
+
+        console.log("result", result);
+        setData(result);
+      };
+
+      fetchData();
+    }, [locale, slug]);
+
+    if (!data) return null;
+
+    return <ArticlePage data={data} />;
 }
